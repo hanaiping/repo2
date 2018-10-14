3d82d9fa914683f1b0190b15e5ae74a965afdbd1
hanaiping <hanaiping@qq.com>
2018-10-14 18:21:08 +0800

    添加testSecond类

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,13 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        //这是注释
+        int i=1;
+        System.out.println(i);
+    }
+
+    @Test
+    public void testSecond(){
+        System.out.println("testSecond");
     }
 }
